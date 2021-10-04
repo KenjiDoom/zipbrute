fcd2d9d22d0e1a9dc4cccaa2bfdbf4755bad5287
Kenjidoom <vitodevcool@protonmail.com>
2021-10-04 12:36:14 -0700

    CODE ADDED

--- zcrack.py
+++ zcrack.py
@@ -1,6 +1,29 @@
+import zipfile 
+import argparse
+import os
+
+
+def keywords(args=None):
+	print("""
+ _______ _ __ __ _  ___| | __
+|_  / __| '__/ _` |/ __| |/ /
+ / / (__| | | (_| | (__|   <
+/___\___|_|  \__,_|\___|_|\_\\
+""")
+	parser = argparse.ArgumentParser(description='Brutforcing Zip files')
+	parser.add_argument('wordlist', metavar='-w', type=str, help='Password list file, must be txt file.')
+	parser.add_argument('zip', metavar='-z', type=str, help='Zip file location')
+	return parser.parse_args(args)
 
 
 def main():
-	pass
+	args = keywords()
+	if os.path.exists(args.wordlist):
+		if os.path.exists(args.zip):
+			print('Also working')
+	else:
+		print('Nada')
 
 
+if __name__ == '__main__':
+	main()
